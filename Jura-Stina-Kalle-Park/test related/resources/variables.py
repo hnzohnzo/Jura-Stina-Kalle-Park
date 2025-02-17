ac10714b641aeae65c3f47002098d369e191306c
MoaRosvallMT24 <moa.rosvall@iths.se>
2025-02-17 10:16:08 +0100

    New files for User Case 2

---
+++ Jura-Stina-Kalle-Park/test related/resources/variables.py
@@ -0,0 +1,28 @@
+import os
+ 
+current_directory = os.getcwd().replace('\\', '/')
+
+# Invalid Credentials
+invalid_username = "woopdiwoop"
+invalid_password = "soupdisoup"
+
+# Timeouts
+standard_timeout = "10s"
+ 
+# Jura specific
+browser_jura = "chrome"
+url_jura = f"file:///{current_directory}/webpages/jurap.html"
+title_jura = "Login Demo"
+
+username_input_id_demo = "id=username"
+password_input_id_demo = "id=password"
+login_button_demo = "id=login-button"
+
+valid_username_demo = "test"
+valid_password_demo = "test"
+
+error_message_element_demo = "class=error-message"
+error_message_demo = "Invalid username or password"
+
+successful_message_element_demo = "id=login-successful-message"
+successful_message_demo = "Welcome test!"
---
+++ Jura-Stina-Kalle-Park/test related/tests/common/stina-palleticket.robot
@@ -0,0 +1,4 @@
+*** Settings ***
+Documentation    Test cases related to registration and login
+Library    SeleniumLibrary
+Library    
